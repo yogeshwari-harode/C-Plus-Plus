98a491568028d1f472e1546956a9eedce836a3f8
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2020-11-25 13:49:03 +0530

    Create Print any table.cpp

---
+++ Print any table.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    
+	for(int i=1;i<=10;i++)
+    {
+    	cout<<n<<"*"<<i<<" = "<<n*i<<endl;
+	}
+	cout<<endl;
+    return 0;
+    
+}
