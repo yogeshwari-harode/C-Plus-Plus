00625ad13893a8d8920c54aafd86c3bda737ab0b
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2020-12-09 10:54:37 +0530

    Create Vector.cpp

---
+++ Vector.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	vector<int>arr(10);
+	for(int i=0;i<6;i++)
+		cin>>arr[i];
+	for(int i=0;i<6;i++)
+		cout<<" "<<arr[i];
+	return 0;
+}
